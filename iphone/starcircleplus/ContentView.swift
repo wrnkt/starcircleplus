5cf7b2dcf1980729cbd5989bedde50bef6da9c9a
Niko <wrnkt1@gmail.com>
2022-12-15 15:52:58 -0500

    Organize iPhone app to folder

---
+++ iphone/starcircleplus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  starcircleplus
+//
+//  Created by niko on 7/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("aha")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iphone/starcircleplus/starcircleplusApp.swift
@@ -0,0 +1,17 @@
+//
+//  starcircleplusApp.swift
+//  starcircleplus
+//
+//  Created by niko on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct starcircleplusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
