44b1f8873a86c00694877ddc4e89f17c713c49cb
Niko <wrnkt1@gmail.com>
2022-12-25 12:51:56 -0500

    Work on check if DB exists and create

--- web/cli/DBSetup.java
+++ web/cli/DBSetup.java
@@ -2,6 +2,7 @@ import java.sql.*;
 
 public class DBSetup
 {
+    private static String DBName = "Test";
     private Connection conn;
 
     public DBSetup()
@@ -17,10 +18,70 @@ public class DBSetup
             System.out.println(e);
         }
     }
+
+    public void createAppDBIfNone()
+    {
+        try
+        {
+            var sql = String.format("SHOW DATABASES like '%s'", DBName);
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next())
+            {
+                System.out.println(String.format("%s database exists.", DBName));
+            }
+            else
+            {
+                System.out.println(String.format("%s database doesn't exist.", DBName));
+                // createAppDB();
+            }
+
+        } catch (SQLException e) {
+        } 
+    }
+
+    public void createAppDB()
+    {
+        try
+        {
+            var sql = String.format("CREATE DATABASE %s", DBName);
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(String.format("Failed to create %s DB", DBName));
+        } 
+            System.out.println(String.format("Created database %s", DBName));
+            
+    }
+
+    public void enterAppDB()
+    {
+        try
+        {
+            var sql = "SELECT DATABASE()";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next())
+            {
+                System.out.println(rs.getString("database()"));
+            }
+            else
+            {
+                System.out.println(String.format("Not in DB %s", DBName));
+            }
+
+        } catch (SQLException e) {
+        } 
+    }
+    
     
     public static void main(String[] args)
     {
         DBSetup dbs = new DBSetup();
+        dbs.enterAppDB();
         System.out.println("DBSetup running");
     }
+
 }
