53252ee52c1e5de5c77965a230f6479cb2d406da
Niko <wrnkt1@gmail.com>
2022-12-25 17:25:46 -0500

    Remove redundant functions moved to DBTableSetup

--- web/cli/DBEntryManager.java
+++ web/cli/DBEntryManager.java
@@ -36,47 +36,6 @@ public class DBEntryManager
         }
     }
 
-    public void createIfNoEntriesTable()
-    {
-        try
-        {
-            var sql = "SHOW tables like 'Entries'";
-            PreparedStatement statement = conn.prepareStatement(sql);
-            ResultSet rs = statement.executeQuery();
-            
-            if (rs.next())
-            {
-                System.out.println("Entries table exists.");
-            }
-            else
-            {
-                System.out.println("Entries table doesn't exist.");
-                createEntriesTable();
-                System.out.println("Created Entries table.");
-            }
-
-        } catch (SQLException e)
-        {
-            System.out.println("Failed to execute show table query.");
-            System.out.println(e);
-        }
-    }
-
-    public void createEntriesTable()
-    {
-        try
-        {
-            var sql = "CREATE TABLE Entries(ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, TYPE BIT(2), CONTENT VARCHAR(255), DATECREATED TIMESTAMP, CERTAINOFDATE BOOLEAN)";
-            PreparedStatement statement = conn.prepareStatement(sql);
-            int result = statement.executeUpdate();
-
-        } catch (SQLException e)
-        {
-            System.out.println("Failed to create Entries table.");
-            System.out.println(e);
-        }
-    }
-
     public void insertEntry(Entry entry) throws Exception
     {
         var sql = "INSERT into Entries (TYPE, CONTENT, DATECREATED, CERTAINOFDATE) values (b?, ?, ?, ?)";
