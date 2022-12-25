4c7a206d7f5412e0224e84bb56764b44362f4cb6
Niko <wrnkt1@gmail.com>
2022-12-25 12:25:02 -0500

    Add DBSetup and DBConfig

---
+++ web/cli/DBConfig.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public final class DBConfig
+{
+    public static final String database = "starcircleplus";
+    public static final String address = "jdbc:mysql://localhost:6603/";
+    // public final String address = String.format("jdbc:mysql://localhost:6603/%s", database);
+    public static final String user = "root";
+    public static final String pass = "makeitwork";
+}
--- web/cli/DBSetup.java
+++ web/cli/DBSetup.java
@@ -2,7 +2,25 @@ import java.sql.*;
 
 public class DBSetup
 {
+    private Connection conn;
+
     public DBSetup()
     {
+        try
+        {
+            conn = DriverManager.getConnection(DBConfig.address, DBConfig.user, DBConfig.pass);
+            System.out.println("Connection established with authentication.");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Unable to connect to database.");
+            System.out.println(e);
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        DBSetup dbs = new DBSetup();
+        System.out.println("DBSetup running");
     }
 }
