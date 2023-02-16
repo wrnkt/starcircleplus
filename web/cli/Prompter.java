a3a400a29ff081f63ccb1f77f2e15e523a92fc59
Niko <wrnkt1@gmail.com>
2023-02-16 11:28:35 -0500

    Move Prompter Entry display formatting into Prompter

--- web/cli/Prompter.java
+++ web/cli/Prompter.java
@@ -103,3 +103,31 @@ public class Prompter
         
     }
 }
+
+
+class PrompterEntryFormatter implements Formatter
+{
+    public static String detailEntry(Entry e)
+    {
+        return shortEntry(e) + "\n" + formatTagList(e);
+    }
+
+    public static String shortEntry(Entry e)
+    {
+        return String.join(" ", e.getIdentifier(), ":", e.getContent());
+    }
+
+    public static String formatTagList(Entry e)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tags:");
+        for(String tag : e.getTagList())
+        {
+            sb.append(" #");
+            sb.append(tag);
+        }
+
+        return sb.toString();
+    }
+}
+
