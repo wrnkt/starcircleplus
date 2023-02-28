527b2762cd174ef5cddb558e4b12069369d1fd40
Niko <wrnkt1@gmail.com>
2023-02-28 14:15:07 -0500

    Initial commit Spring REST service

---
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/StarcircleplusApplication.java
@@ -0,0 +1,13 @@
+package com.tanchee.starcircleplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarcircleplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarcircleplusApplication.class, args);
+	}
+
+}
---
+++ web/backend/starcircleplus/src/test/java/com/tanchee/starcircleplus/StarcircleplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanchee.starcircleplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarcircleplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
