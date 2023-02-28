29030ccfe0b68f0aae8b97445a718cee76f2d8c0
Niko <wrnkt1@gmail.com>
2023-02-28 17:36:07 -0500

    Redefine authorization with roles

--- web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/SecurityConfig.java
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/SecurityConfig.java
@@ -3,8 +3,11 @@ package com.tanchee.starcircleplus;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 public class SecurityConfig
@@ -12,13 +15,14 @@ public class SecurityConfig
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
-        // security.httpBasic().disable(); // only works for GET
-        // http.csrf().disable().authorizeRequests().anyRequest().permitAll(); // works for GET, POST, PUT, DELETE
-        
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll())
-            .httpBasic();
+        http
+            .authorizeHttpRequests((authz) -> authz
+                    .requestMatchers("/", "/home", "/greeting").permitAll()
+                    .requestMatchers("/user").access(new WebExpressionAuthorizationManager("hasRole('USER')"))
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/db/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') and hasRole('DBA')"))
+                    .anyRequest().denyAll());
 
         return http.build();
-                                                                            
     }
 }
