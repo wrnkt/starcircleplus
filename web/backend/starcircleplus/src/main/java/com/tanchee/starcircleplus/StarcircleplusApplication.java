fbdbe68581aa23bdb74c39401640bd41aea1a9d1
Niko <wrnkt1@gmail.com>
2023-03-01 08:11:03 -0500

    Fix ComponentScan location and inability to find Entity Bean Component

--- web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/StarcircleplusApplication.java
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/StarcircleplusApplication.java
@@ -3,9 +3,16 @@ package com.tanchee.starcircleplus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class StarcircleplusApplication {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = { "com.tanchee.starcircleplus.*" })
+@ComponentScan(basePackages = "com.tanchee.starcircleplus.*")
+@EntityScan("com.tanchee.starcircleplus.*")
+public class StarcircleplusApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(StarcircleplusApplication.class, args);
 	}
