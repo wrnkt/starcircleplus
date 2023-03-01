83f2e4f7b4a8e99769ab6f1d6ef77e802e2470a7
Niko <wrnkt1@gmail.com>
2023-02-28 23:36:23 -0500

    Fix Entry return values

--- web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/EntryController.java
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/EntryController.java
@@ -6,28 +6,53 @@ import java.time.ZonedDateTime;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class EntryController
 {
+    // private ApplicationContext context = new AnnotationConfigApplicationContext(EntryConfig.class);
+
+    // EntryRepository repo = context.getBean(EntryRepository.class);
+
+    @Autowired private EntryService entryService;
+
+    // NOTE: Test values.
     private final AtomicLong entryID = new AtomicLong();
     private final long uID = 1L;
     private static final String content = "Placeholder entry text.";
     private static final ArrayList<String> tags = new ArrayList(Arrays.asList("tag1", "tag2"));
     private static final ZonedDateTime dateCreated = ZonedDateTime.now();
 
+    //EntryEntity
     @GetMapping("/testentry")
     public Entry getTestEntry()
     {
-        return new Entry(entryID.incrementAndGet(), uID, Type.STAR, true, dateCreated, tags);
+        return new Entry(uID, Type.STAR, true, dateCreated, tags);
     }
 
-    @PostMapping("/testentry")
-    public Entry postEntry()
+    @PostMapping("/saveentry")
+    public Entry saveEntry(@Valid @RequestBody Entry entry)
     {
+        Entry testEntry = new Entry(
+                1L,
+                Type.STAR,
+                true,
+                ZonedDateTime.now(),
+                new ArrayList(Arrays.asList("tag1", "tag2"))
+                );
+        // testEntry = repo.save(testEntry);
+        // TEST: FOR TESTING
+        return entryService.saveEntry(testEntry);
     }
 }
