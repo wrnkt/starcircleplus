776413c58bce4f004c51d06cfc5c553f31b2bef5
Niko <wrnkt1@gmail.com>
2023-03-01 01:07:46 -0500

    Remove updateEntry. Add Component annotation. Add future table reference import.

--- web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/EntryService.java
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/EntryService.java
@@ -1,14 +1,23 @@
 package com.tanchee.starcircleplus;
 
+import java.util.List;
+
+import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 
+// import org.springframework.data.relational.core.mapping.Table;
+
+@Component
 public interface EntryService
 {
     Entry saveEntry(Entry entry);
 
     List<Entry> fetchEntryList();
 
+    // TODO: Implement updating Entries in EntryServiceImplementation
+    /*
     Entry updateEntry(Entry entry, Long entryID);
+    */
 
     void deleteEntryByID(Long entryID);
 }
