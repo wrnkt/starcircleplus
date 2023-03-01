a2e5cf2f1f7752aaabf91dcf5960401e9c8bef2c
Niko <wrnkt1@gmail.com>
2023-02-28 23:49:53 -0500

    Possible Config implementation

--- web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/EntryConfig.java
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/EntryConfig.java
@@ -17,3 +17,9 @@ public class EntryConfig
         return new EntryRepository();
       }
 }
+
+[12,16] constructor EntryServiceImplementation in class com.tanchee.starcircleplus.EntryServiceImplementation cannot be applied to gi
+ven types;
+[ERROR]   required: no arguments
+[ERROR]   found:    com.tanchee.starcircleplus.EntryRepository
+[ERROR]   reason: actual and formal argument lists differ in length
