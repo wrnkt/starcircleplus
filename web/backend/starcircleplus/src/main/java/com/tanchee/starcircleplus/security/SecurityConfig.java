719dc5397db149798e5703e732b5b03c1752b6b1
Niko <wrnkt1@gmail.com>
2023-03-02 15:13:45 -0500

    Remove security roles

--- web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/security/SecurityConfig.java
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/security/SecurityConfig.java
@@ -4,24 +4,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig
 {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
         http
-            .authorizeHttpRequests((authz) -> authz
-                    .requestMatchers("/", "/home", "/user/**").permitAll()
+            .authorizeHttpRequests((requests) -> requests
+                    .requestMatchers("/", "/home", "/user/**").permitAll().anyRequest()
                     //.requestMatchers("/user").access(new WebExpressionAuthorizationManager("hasRole('USER')"))
-                    .requestMatchers("/admin/**").hasRole("ADMIN")
-                    .requestMatchers("/db/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') and hasRole('DBA')"))
-                    .anyRequest().denyAll());
+                    //.requestMatchers("/admin/**").hasRole("ADMIN")
+                    //.requestMatchers("/db/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') and hasRole('DBA')"))
+                    //.anyRequest().denyAll()
+            )
+            //.logout((logout) -> logout.permitAll())
+            .httpBasic(withDefaults());
 
         return http.build();
     }
