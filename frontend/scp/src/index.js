9bcf8f3b8da151ae788eabe70621e0e0273a837f
Niko <wrnkt1@gmail.com>
2023-03-03 16:37:56 -0500

    Display App view. Add color note. Add test entry.

--- frontend/scp/src/index.js
+++ frontend/scp/src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+import App from './App.js';
 import Entry from './Entry.js';
+import Table from './Table.js';
+
+const testEntry = {
+        "type": "STAR",
+        "checked": false,
+        "dateCreated": "2023-03-03T14:04:21.374638-05:00",
+        "content": "new entry"
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Entry />
+    <App />
   </React.StrictMode>
 );
+
+// #34495E
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
