0647cc083f8a3ba371a761722cbc11307456d4c4
Niko <wrnkt1@gmail.com>
2023-03-02 23:22:35 -0500

    Return Entry from adding Entry. Remove test values.

--- web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryDataTransferController.java
+++ web/backend/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryDataTransferController.java
@@ -32,16 +32,9 @@ public class EntryDataTransferController
     @Autowired
     private EntryRepository entryRepository;
 
-    // NOTE: Test values.
-    private final AtomicLong entryID = new AtomicLong();
-    private final long uID = 1L;
-    private static final String content = "Placeholder entry text.";
-    private static final ArrayList<String> tags = new ArrayList(Arrays.asList("tag1", "tag2"));
-    private static final ZonedDateTime dateCreated = ZonedDateTime.now();
-
 
     @GetMapping(path="/test")
-    public @ResponseBody EntryDataTransfer getTestEntry()
+    public @ResponseBody EntryDataTransfer getTestEntryDataTransfer()
     {
         return new EntryDataTransfer(uID, Type.STAR, true, dateCreated, tags, content);
     }
@@ -54,7 +47,7 @@ public class EntryDataTransferController
 
     // NOTE: Add @Valid before @RequestBody
     @PostMapping(path="/save")
-    public EntryDataTransfer addEntry(@RequestBody EntryDataTransfer entry)
+    public Entry addEntry(@RequestBody EntryDataTransfer entry)
     {
         EntryDataTransfer newEntryDataTransfer = new EntryDataTransfer(
                 1L,
@@ -65,15 +58,17 @@ public class EntryDataTransferController
                 entry.getContent()
         );
 
-        entryRepository.save(
-                new Entry(
-                    newEntryDataTransfer.getType(),
-                    newEntryDataTransfer.getChecked(),
-                    newEntryDataTransfer.getDateCreated(),
-                    newEntryDataTransfer.getContent()
-                )
+        Entry newEntry = new Entry(
+                newEntryDataTransfer.getType(),
+                newEntryDataTransfer.getChecked(),
+                newEntryDataTransfer.getDateCreated(),
+                newEntryDataTransfer.getContent()
         );
 
-        return newEntryDataTransfer;
+        entryRepository.save(
+                newEntry
+        );
+
+        return newEntry;
     }
 }
