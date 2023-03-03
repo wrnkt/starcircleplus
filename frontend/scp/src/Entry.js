333bc9e8a4dbc6e084a606e9a7ec36deb6761d68
Niko <wrnkt1@gmail.com>
2023-03-03 15:11:15 -0500

    Create & display entry React component

---
+++ frontend/scp/src/Entry.js
@@ -0,0 +1,11 @@
+import './Entry.css';
+
+function Entry() {
+  return (
+    <div className="Entry">
+      Test test test
+    </div>
+  );
+}
+
+export default Entry;
--- frontend/scp/src/index.js
+++ frontend/scp/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Entry from './Entry.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Entry />
   </React.StrictMode>
 );
 
