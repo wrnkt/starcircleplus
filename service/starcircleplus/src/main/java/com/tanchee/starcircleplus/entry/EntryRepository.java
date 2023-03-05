33af967ceb7f7b37a46afbf34e7caa3ce02affde
Niko <wrnkt1@gmail.com>
2023-03-05 07:26:19 -0500

    Add function to find list of Entries with associated Tag

--- service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryRepository.java
+++ service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryRepository.java
@@ -1,5 +1,9 @@
 package com.tanchee.starcircleplus.entry;
 
+import com.tanchee.starcircleplus.tag.Tag;
+
+import java.util.List;
+
 //import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EntryRepository extends CrudRepository<Entry, Long>
 {
+    List<Entry> findByTagsEquals(Tag tag);
 }
