85720fd37f7f127d948e7f7552f0ca52cb02e4d6
Niko <wrnkt1@gmail.com>
2023-03-06 11:46:20 -0500

    Get data from server and display. Add entry format reference & notes.

--- react-native/StarCirclePlus/App.tsx
+++ react-native/StarCirclePlus/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   View,
@@ -8,26 +8,40 @@ import {
   StatusBar,
 } from 'react-native';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    type: 'STAR',
-    dateCreated: "2023-03-05T11:37:02.545883-05:00",
-    content: 'First Entry',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    type: 'CIRCLE',
-    dateCreated: "2023-03-05T11:37:27.100338-05:00",
-    content: 'Second Entry',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    type: 'PLUS',
-    dateCreated: "2023-03-05T11:37:37.155832-05:00",
-    content: 'Third Entry',
-  },
+/*
+// TODO: Add ID from table for unique key prop
+const ENTRYDATAFORMAT = [
+    {
+        "type": "PLUS",
+        "checked": false,
+        "dateCreated": "2023-03-06T11:40:39.321445-05:00",
+        "tags": [
+            "listview",
+            "iphonetest"
+        ],
+        "content": "test again"
+    }
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        type: 'STAR',
+        dateCreated: "2023-03-05T11:37:02.545883-05:00",
+        content: 'First Entry',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        type: 'CIRCLE',
+        dateCreated: "2023-03-05T11:37:27.100338-05:00",
+        content: 'Second Entry',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        type: 'PLUS',
+        dateCreated: "2023-03-05T11:37:37.155832-05:00",
+        content: 'Third Entry',
+    },
 ];
+*/
+
 
 const EntryTypeDisplay = ({type, checked}) => (
         <View>
@@ -56,15 +70,37 @@ const Entry = ({entry}) => (
 );
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Entry entry={item} />}
-        keyExtractor={entry => entry.id}
-      />
-    </SafeAreaView>
-  );
+    const [data,setData]=useState([]);
+    const getData=()=>{
+        fetch('http://localhost:8080/entry/all',
+                {
+                    headers : { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                    }
+                }
+        )
+        .then(function(response){
+            console.log(response)
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(myJson);
+            setData(myJson)
+        });
+    }
+    useEffect(()=>{
+        getData()
+        },[])
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={data}
+                renderItem={({item}) => <Entry entry={item} />}
+                keyExtractor={entry => entry.id}
+            />
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
