cd225d76ba311e2a0860dc03a0c4db05ac854605
Niko <wrnkt1@gmail.com>
2023-03-20 03:57:46 -0400

    Switch to JpaRepository

--- service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryRepository.java
+++ service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryRepository.java
@@ -4,12 +4,18 @@ import com.tanchee.starcircleplus.tag.Tag;
 
 import java.util.List;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EntryRepository extends CrudRepository<Entry, Long>
+public interface EntryRepository extends JpaRepository<Entry, Long>
 {
     List<Entry> findByTagsEquals(Tag tag);
+
+    /*
+    @Modifying
+@Query("update Entry e set e.type = ?1, e.checked = ?2, e.content = ?3, e.tags = ?4 where e.id = ?5")
+    void setEntryById(Type type, boolean checked, String content, Set<Tags> tags, Long id);
+    */
 }
