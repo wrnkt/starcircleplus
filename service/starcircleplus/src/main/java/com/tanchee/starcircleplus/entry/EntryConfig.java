0816a9a7f3b256b890f55374ceaddc8a7ab369ee
Niko <wrnkt1@gmail.com>
2023-03-20 23:49:33 -0400

    Clean EntryConfig

--- service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryConfig.java
+++ service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryConfig.java
@@ -3,27 +3,10 @@ package com.tanchee.starcircleplus.entry;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+
 @Configuration
 public class EntryConfig
 {
-    /*
-      @Bean
-      public EntryService entryService()
-      {
-        return new EntryService(entryRepository());
-      }
-
-      @Bean
-      public EntryRepository entryRepository() {
-        return new EntryRepository();
-      }
-      */
 }
-
-/*
-[12,16] constructor EntryServiceImplementation in class com.tanchee.starcircleplus.EntryServiceImplementation cannot be applied to gi
-ven types;
-[ERROR]   required: no arguments
-[ERROR]   found:    com.tanchee.starcircleplus.EntryRepository
-[ERROR]   reason: actual and formal argument lists differ in length
-*/
