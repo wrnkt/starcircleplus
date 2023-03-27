02db0d0c3080b070ffdbd86096c1adbc2966884e
Niko <wrnkt1@gmail.com>
2023-03-27 03:19:50 -0400

    Implement BaseJpaRepository

--- service/starcircleplus/src/main/java/com/tanchee/starcircleplus/tag/TagRepository.java
+++ service/starcircleplus/src/main/java/com/tanchee/starcircleplus/tag/TagRepository.java
@@ -6,15 +6,21 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 //import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import io.hypersistence.utils.spring.repository.BaseJpaRepository;
+
 @Repository
-public interface TagRepository extends JpaRepository<Tag, Long>
+public interface TagRepository extends BaseJpaRepository<Tag, Long>
 {
     List<Tag> findByNameEquals(String name);
 
     Optional<Tag> findByName(String name);
 
     List<Tag> findByEntryEquals(Entry entry);
+
+    @Query("from Tag")
+    List<Tag> findAll();
 }
