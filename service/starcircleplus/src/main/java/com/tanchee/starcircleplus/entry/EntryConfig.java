030bcda4ea098fef3a701dfa5b9307151606fa79
Niko <wrnkt1@gmail.com>
2023-03-27 02:16:39 -0400

    Configure JPA Repositories at application package level

--- service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryConfig.java
+++ service/starcircleplus/src/main/java/com/tanchee/starcircleplus/entry/EntryConfig.java
@@ -2,18 +2,8 @@ package com.tanchee.starcircleplus.entry;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import io.hypersistence.utils.spring.repository.BaseJpaRepositoryImpl;
-
 
 @Configuration
-@EnableJpaRepositories(
-    value = {"com.tanchee.starcircleplus.entry",
-             "com.tanchee.starcircleplus.tag",
-             "com.tanchee.starcircleplus.user"}, 
-    repositoryBaseClass = BaseJpaRepositoryImpl.class
-)
 public class EntryConfig
 {
 }
