6a6560bdaedac74a0ae1bc13f23183a6d74b228a
Niko <wrnkt1@gmail.com>
2023-04-06 13:17:08 -0400

    Display type and checked status in ItemRow

--- ios-mongo-swift/App/Views/ItemRow.swift
+++ ios-mongo-swift/App/Views/ItemRow.swift
@@ -6,16 +6,37 @@ struct ItemRow: View {
     
     var body: some View {
         NavigationLink(destination: ItemDetail(item: item)) {
+            switch item.type {
+                case EntryType.todo:
+                    if item.isComplete {
+                        Image(systemName: "checkmark.circle")
+                    } else {
+                        Image(systemName: "circle")
+                    }
+                case EntryType.note:
+                    Image(systemName: "plus")
+                case EntryType.special:
+                    Image(systemName: "star.fill")
+            }
+            Spacer()
             Text(item.textContent)
             Spacer()
             if item.isComplete {
+                /*
                 Image(systemName: "checkmark")
                     .foregroundColor(.blue)
                     .padding(.trailing, 10)
+                 */
             }
             if item.owner_id == app.currentUser?.id {
-                Text("(mine)")
+                //Text("(mine)")
             }
         }
+    }
+}
+
+struct Previews_ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
