b7d34d26e6b335a38ce11afa91a56d47767926fe
Niko <wrnkt1@gmail.com>
2023-04-06 13:30:40 -0400

    Extract TypeDisplay into separate view

--- ios-mongo-swift/App/Views/ItemRow.swift
+++ ios-mongo-swift/App/Views/ItemRow.swift
@@ -6,31 +6,33 @@ struct ItemRow: View {
     
     var body: some View {
         NavigationLink(destination: ItemDetail(item: item)) {
-            switch item.type {
-                case EntryType.todo:
-                    if item.isComplete {
-                        Image(systemName: "checkmark.circle")
-                    } else {
-                        Image(systemName: "circle")
-                    }
-                case EntryType.note:
-                    Image(systemName: "plus")
-                case EntryType.special:
-                    Image(systemName: "star.fill")
-            }
+            TypeDisplay(item: item)
             Spacer()
             Text(item.textContent)
             Spacer()
-            if item.isComplete {
-                /*
-                Image(systemName: "checkmark")
-                    .foregroundColor(.blue)
-                    .padding(.trailing, 10)
-                 */
-            }
+            
             if item.owner_id == app.currentUser?.id {
                 //Text("(mine)")
             }
+        }
+    }
+}
+
+struct TypeDisplay: View {
+    @ObservedRealmObject var item: Item
+    
+    var body: some View {
+        switch item.type {
+            case EntryType.todo:
+                if item.isComplete {
+                    Image(systemName: "checkmark.circle")
+                } else {
+                    Image(systemName: "circle")
+                }
+            case EntryType.note:
+                Image(systemName: "plus")
+            case EntryType.special:
+                Image(systemName: "star.fill")
         }
     }
 }
