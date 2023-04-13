39b3a73c2527c678d2f453136db908e703b7c9eb
Niko <wrnkt1@gmail.com>
2023-04-12 21:07:56 -0400

    Add Tags and ownerId.

--- ios-mongo-swift/App/Item.swift
+++ ios-mongo-swift/App/Item.swift
@@ -2,16 +2,18 @@ import RealmSwift
 
 class Item: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var owner_id: String
+    @Persisted var type: EntryType
     @Persisted var isComplete = false
     @Persisted var textContent: String
-    @Persisted var type: EntryType
-    @Persisted var owner_id: String
+    @Persisted var tags: List<Tag>
 }
 
 enum EntryType: Int, PersistableEnum, CaseIterable {
     case todo = 0
     case special = 1
     case note = 2
+    
     
     var description: String {
         switch self {
@@ -20,4 +22,11 @@ enum EntryType: Int, PersistableEnum, CaseIterable {
         case .note: return "Plus"
         }
     }
+}
+
+class Tag: Object, ObjectKeyIdentifiable {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var owner_id: String
+    @Persisted var name: String
+    @Persisted(originProperty: "tags") var item: LinkingObjects<Item>
 }
