b132eaa260201aee06346e16904d96793ce2792e
Niko <wrnkt1@gmail.com>
2023-04-12 21:44:42 -0400

    Create entries for testing and preview.

--- ios-mongo-swift/App/Item.swift
+++ ios-mongo-swift/App/Item.swift
@@ -1,4 +1,5 @@
 import RealmSwift
+import UIKit
 
 class Item: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var _id: ObjectId
@@ -7,6 +8,37 @@ class Item: Object, ObjectKeyIdentifiable {
     @Persisted var isComplete = false
     @Persisted var textContent: String
     @Persisted var tags: List<Tag>
+ 
+    //// TEST ////
+    static func testNote() -> Item {
+        let testNote = Item()
+        // testNote._id = 0
+        // testNote.owner_id = ""
+        testNote.type = EntryType.note
+        testNote.isComplete = false
+        testNote.textContent = "This is a note Entry."
+        return testNote
+    }
+    
+    static func testTodo() -> Item {
+        let testTodo = Item()
+        // testTodo._id = 0
+        // testTodo.owner_id = ""
+        testTodo.type = EntryType.todo
+        testTodo.isComplete = false
+        testTodo.textContent = "This is a todo Entry."
+        return testTodo
+    }
+    
+    static func testSpecial() -> Item {
+        let testSpecial = Item()
+        // testSpecial._id = 0
+        // testSpecial.owner_id = ""
+        testSpecial.type = EntryType.special
+        testSpecial.isComplete = false
+        testSpecial.textContent = "This is a special Entry."
+        return testSpecial
+    }
 }
 
 enum EntryType: Int, PersistableEnum, CaseIterable {
@@ -30,3 +62,4 @@ class Tag: Object, ObjectKeyIdentifiable {
     @Persisted var name: String
     @Persisted(originProperty: "tags") var item: LinkingObjects<Item>
 }
+
